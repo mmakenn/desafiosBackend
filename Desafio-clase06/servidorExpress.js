b120f80f1e746a3ffcbda672a4a9bb4853e796c6
Mac Meldi Roch <mmeldiroch@gmail.com>
2022-04-14 11:13:46 -0300

    Subo de nuevo todo el repo

---
+++ Desafio-clase06/servidorExpress.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { Contenedor } = require('./contenedor');
+
+const app = express();
+const PORT = 8080;
+
+const archivoProductos = new Contenedor('productos.txt');
+
+app.get('/', (req, res) => {
+    res.send('Ruta inválida (por ahora...)');
+});
+
+app.get('/productos', (req, res) => {
+    showProducts(res);
+});
+
+app.get('/productoRandom', (req, res) => {
+    showOneProduct(res);
+});
+
+async function showProducts(res) {
+    const productos = await archivoProductos.getAll();
+    res.send(productos);
+}
+
+async function showOneProduct(res) {
+    const producto = await archivoProductos.getById(3);
+    res.send(producto);
+}
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto: ${server.address().port}`);
+});
+server.on("error", error => console.log(`Error en servidor: ${error}`));
